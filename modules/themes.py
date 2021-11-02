a7886da32f243d87d5b4b7c7dfed9ea840478400
Adrian Granados <adrian@intuitibits.com>
2021-11-02 13:45:06 -0400

    Adds support for "themes."
    Changes mode to RGB and uses pretty colors for titles, etc. Also,
    "themes" can be defined and in the future we might even be able
    to add an option to select a theme.

---
+++ modules/themes.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#
+"""
+themes.py - display themes
+"""
+
+from enum import Enum
+from modules.env_utils import EnvUtils
+
+env_util = EnvUtils()
+PLATFORM = env_util.get_platform()
+
+class BlackAndWhiteTheme(Enum):
+    text_foreground               = "white"
+    text_background               = "black"
+    page_title_background         = "white"
+    page_title_foreground         = "black"
+    page_item_foreground          = "white"
+    page_item_background          = "black"
+    page_selected_item_foreground = "black"
+    page_selected_item_background = "white"
+    page_table_title_foreground   = "black"
+    page_table_title_background   = "white"
+    page_table_row_foreground     = "white"
+    page_table_row_background     = "black"
+    simple_table_title_foreground = "black"
+    simple_table_title_background = "white"
+    simple_table_row_foreground   = "white"
+    simple_table_row_background   = "black"
+
+class ProTheme(Enum):
+    text_foreground               = "white"
+    text_background               = "black"
+    page_title_foreground         = "white"
+    page_title_background         = "#205493"
+    page_item_foreground          = "white"
+    page_item_background          = "black"
+    page_selected_item_foreground = "black"
+    page_selected_item_background = "#f9c642"
+    page_table_title_foreground   = "black"
+    page_table_title_background   = "#f9c642"
+    page_table_row_foreground     = "white"
+    page_table_row_background     = "black"
+    simple_table_title_foreground = "black"
+    simple_table_title_background = "#f9c642"
+    simple_table_row_foreground   = "white"
+    simple_table_row_background   = "black"
+
+if PLATFORM == "pro":
+    THEME = ProTheme
+else:
+    THEME = BlackAndWhiteTheme
