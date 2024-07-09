9ae6da6dc027f93e35036a622509b7dffa1e20d9
Adrian Granados <adrian@intuitibits.com>
2024-07-09 12:00:02 -0400

    Reverting latest change.

--- fpms/modules/wlanpi_oled.py
+++ fpms/modules/wlanpi_oled.py
@@ -2,37 +2,29 @@
 # -*- coding: utf-8 -*-
 
 import os
-from fpms.modules.constants import PLATFORM, DISPLAY_TYPE
-from fpms.modules.display import *
-from fpms.modules.platform import *
+from fpms.modules.constants import DISPLAY_TYPE, DISPLAY_TYPE_ST7735
+from fpms.modules.screen.st7735 import ST7735
+from fpms.modules.screen.luma import Luma
 
-# Initialize device to None
-device = None
+# Initialize device based on the display type
+device = ST7735() if DISPLAY_TYPE == DISPLAY_TYPE_ST7735 else Luma()
 
-# Path to GPIO sysfs
-gpio_sysfs_path = "/sys/class/gpio"
-
-# Check if GPIO sysfs path exists and if the display type is ST7735
-if os.path.exists(gpio_sysfs_path) and DISPLAY_TYPE == DISPLAY_TYPE_ST7735:
-    # Use Waveshare's proprietary code to drive the ST7735 screen module
-    from fpms.modules.screen.st7735 import ST7735
-    device = ST7735()
-else:
-    # Default to Luma for driving the screen if not using a different implementation
-    from fpms.modules.screen.luma import Luma
-    device = Luma()
-
+# Initialize the device
 def init():
     device.init()
 
+# Draw an image on the display
 def drawImage(image):
     device.drawImage(image)
 
+# Clear the display
 def clear():
     device.clear()
 
+# Put the display to sleep
 def sleep():
     device.sleep()
 
+# Wake up the display
 def wakeup():
     device.wakeup()
